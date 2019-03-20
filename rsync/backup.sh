d10e6065286c363780e0eb3de6c63976df8e86b7
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2019-03-20 06:52:42 +0100

    rsync: require commandline option -c for the config file

--- rsync/backup.sh
+++ rsync/backup.sh
@@ -1,13 +1,47 @@
 #!/bin/bash
+# SPDX-License-Identifier: GPL-2.0+
+# Copyright (C) 2019, Martin Kepplinger <martink@posteo.de>
 
-# TODO get config file from argument
+have_config_file=0
 
-if [ ! -f ~/backupconfig.sh ]; then
+args=$(getopt -o c: -- "$@")
+if [ $? -ne 0 ] ; then
+        exit 1
+fi
+eval set -- "$args"
+while [ $# -gt 0 ]
+do
+        case "$1" in
+        -c)
+                CONFIG_FILE=$2
+                have_config_file=1
+                shift
+                ;;
+        --)
+                shift
+                break
+                ;;
+        *)
+                echo "Invalid option: $1"
+                exit 1
+                ;;
+        esac
+        shift
+done
+
+if [ "$have_config_file" -gt 0 ] ; then
+	echo "Using configuration ${CONFIG_FILE} with exlude list ${EXCLUDE_LIST}"
+else
+	echo "Please add -c <configfile>"
+	exit 1
+fi
+
+if [ ! -f ${CONFIG_FILE} ]; then
 	echo "config file not found!"
 	exit 0
 fi
 
-source ~/backupconfig.sh
+source ${CONFIG_FILE}
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
